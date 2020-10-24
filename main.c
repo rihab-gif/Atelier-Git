cb53afd17ee36cd27e2f7d25ae87277eba2bbce3
rihab-gif <rihab.zammali@esprit.tn>
2020-10-24 22:57:25 +0100

    MAJ amin

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio>
+int main() {
+   int a;
+   int b;
+   printf("n\Saisie du premier Nombre:");
+   scanf("%d",&a);
+   do {
+   printf("\nsaisie du Second Nombre:");
+   scanf("%d",&d);
+   }
+   while(b==0);
+   printf("\n%d+%d=%d",a,b,a+b);
+   printf("\n%d-%d=%d",a,b,a-b);
+   printf("\n%d/%d=%f\n",a,b,(float)a/b);
+   return (0);
+   }
+   
